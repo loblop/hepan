9537064a0a3454b7cb38be8b552d86fed5f4641b
Week.D.Awn <weekdawn@qq.com>
2022-05-18 23:17:06 +0800

    init

---
+++ src/components/tou.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tou">
+    <div id="title">这里是 tou</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tou',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.tou{
+    #title{
+        width: 200px;
+        height: 40px;
+        background-color: rgb(0, 221, 255);
+    }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1>title</h1>
+    <tou></tou>
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import tou from '@/components/tou.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    tou
+  }
+}
+</script>
